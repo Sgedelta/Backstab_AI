54b6e0702c9c11439e7f1c32a521d0f1838ca6b3
Samuel Easton (RIT Student) <sge3418@rit.edu>
2026-03-04 11:52:12 -0500

    updated sabre runner with a few more threading details

--- Scenes/SabreRunner.cs
+++ Scenes/SabreRunner.cs
@@ -9,7 +9,9 @@ public partial class SabreRunner : Node
 	GodotThread BashThread;
 	Semaphore BashSema;
 	Mutex BashMutex;
+	bool exitThread = false;
 
+	[Signal] public delegate void SabreCompletedEventHandler();
 
 
 	// Called when the node enters the scene tree for the first time.
@@ -26,5 +28,55 @@ public partial class SabreRunner : Node
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+    public override void _ExitTree()
+    {
+		base._ExitTree();
+		//we have to safely close the thread before we exit the tree
+		ExitThread();
+    }
+
+	/// <summary>
+	/// Frees the running thread - MIGHT cause blocking, but is needed to exit safely
+	/// </summary>
+	public void ExitThread()
+	{
+		BashMutex.Lock();
+		exitThread = true;
+		BashMutex.Unlock();
+
+		BashSema.Post();
+
+		BashThread.WaitToFinish();	
+	}
+
+
+	public void PollSabre()
+	{
+		//keep looping until we are told to break with exitThread
+		while (true)
+		{
+			BashSema.Wait(); //hold until we want to run something
+
+			//check if we've gotten the end signal
+			BashMutex.Lock();
+			bool leave = exitThread;
+			BashMutex.Unlock();
+
+			if(leave)
+			{
+				break;
+			}
+
+			//now do our processing
+
+
+
+
+			//once processing is done, let the main thread know that we are done with a deferred signal call (deferred to resync with main thread. I think.)
+			CallDeferred("emit_signal", "SabreCompleted");
+			
+		}
 	}
 }
